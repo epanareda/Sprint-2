5ff7041c48a732c647545ce8570a48357e0a68b8
epanareda <epanareda@gmail.com>
2022-11-24 22:32:51 +0100

    Nivells 2-3

---
+++ Nivell 2-3/js/main.js
@@ -0,0 +1,22 @@
+// THIS CODE IS EXACTLY THE SAME AS THE ONE GIVEN IN THE OFFICAL BOOTSTRAP WEB PAGE (https://getbootstrap.com/docs/5.0/forms/validation/?)
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
